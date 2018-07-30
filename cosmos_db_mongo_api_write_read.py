e5ab71585be04eee8178605e1a884685847e1e1e
Niels Jespersen <niels@jespersen.info>
2018-07-30 22:24:20 +0200

    Made to work now

--- cosmos_db_mongo_api_write_read.py
+++ cosmos_db_mongo_api_write_read.py
@@ -8,5 +8,8 @@ post = {"author": "Mike",
         "text": "My first blog post!",
         "tags": ["mongodb", "python", "pymongo"],
         "date": datetime.datetime.utcnow()}
-post_id = posts.insert_one(post).inserted_id        
-print(db.collection_names(include_system_collections=False))
+post_id = posts.insert_one(post).inserted_id
+print(posts.find_one({"_id": post_id}))
+
+for post in posts.find():
+    print(post)
