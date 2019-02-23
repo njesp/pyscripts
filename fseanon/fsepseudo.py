732d0f59d0af38b6f265fe354d91785a2fa6ab55
Niels Jespersen <niels@jespersen.info>
2019-02-23 18:53:23 +0100

    beginning work

--- fseanon/fsepseudo.py
+++ fseanon/fsepseudo.py
@@ -2,19 +2,23 @@
 FSE Pseudonymisering, PoC
 """
 import base64
-import hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
 
 
-class AESCipher(object):
+class FSEKryptoDingenot(object):
 
-    def __init__(self, key):
+    def __init__(self):
         self.bs = 32
-        self.key = hashlib.sha256(key.encode()).digest()
-# SHA512 med 256 bit output er state-of-the-art hash-funktion til at generere en 256 bit
-# værdi
-# from Crypto.Hash import SHA512;h = SHA512.new(truncate="256")
+        self.key = None
+
+    def genkey(self, keystr):
+        self.key = SHA256.new(data=keystr.encode()).digest()
+        return base64.encodebytes(s=self.key)
+
+    def setkey(self, keystrbase64):
+        self.key = base64.decodebytes(s=keystrbase64)
 
     def encrypt(self, raw):
         raw = self._pad(raw)
@@ -37,4 +41,21 @@ class AESCipher(object):
 
 
 if __name__ == "__main__":
-    pass
+    pass_phrase = """
+    Da jeg gik ud over Langebro
+    en tidlig mandag morgen
+    da så jeg en der stod og græd.
+    Hvis du tør - så kom med mig.
+    """
+
+    k = FSEKryptoDingenot()
+
+    strk = k.genkey(keystr=pass_phrase)
+    # Gem strk, det er den læsevenlige nøgle. Pass frasen skal kun bruges en
+    # gang initielt. Jo længere jo bedre. strk er printbar udgave af selve nøglen der er
+    # afledt af pass frasen.
+    k1 = k.key
+    k.setkey(keystrbase64=strk)
+    k2 = k.key
+    if k1 == k2:
+        print('xxx')
