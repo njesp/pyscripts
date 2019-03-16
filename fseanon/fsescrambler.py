ad98456efe2e7bcb9de7f83733d2cba762132e47
Niels Jespersen <niels@jespersen.info>
2019-03-16 19:56:23 +0100

    added scrambler

---
+++ fseanon/fsescrambler.py
@@ -0,0 +1,12 @@
+import string
+import pyffx
+
+
+class FseScramble(pyffx.String):
+    def __init__(self, key, length, alphabet=string.punctuation + string.ascii_letters + string.digits):
+        super(FseScramble, self).__init__(ffx=key, length=length, alphabet=alphabet)
+
+
+e = FseScramble(key=b'02sxdfkkallxcffssdfg', length=6)
+print(e.encrypt('abcde'))
+print(e.decrypt('acbacc'))
