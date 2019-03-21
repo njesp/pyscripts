09b3403252ad6d2f10f42362247931b390465bc6
Niels Jespersen <niels@jespersen.info>
2019-03-21 05:35:59 +0100

    more stuff

--- fseanon/gen_data.py
+++ fseanon/gen_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 from Cryptodome.Random import random
 
 df = pd.DataFrame()
-df["pnr"] = [f"{random.randint(1, 1000000000):010d}" for i in range(100)]
+df["pnr"] = [f"{random.randint(1, 1000000000):010d}" for i in range(1000)]
 df["value1"] = "xzz" + df["pnr"] + "zzx"
 df["value2"] = "3$" + df["pnr"] + ":-xm"
 df.to_csv("c:/temp/xx.csv", sep=";", header=False, index=False)
