b30edbe661c2239d9aa303c398cdde57d0266271
Niels Jespersen <niels@jespersen.info>
2019-03-24 11:43:12 +0100

    change settings

--- fseanon/gen_data.py
+++ fseanon/gen_data.py
@@ -1,13 +1,14 @@
+"""
+Generer testdata
+"""
 import pandas as pd
 from Cryptodome.Random import random
 
 df = pd.DataFrame()
-df["pnr"] = [f"{random.randint(1, 1000000000):010d}" for i in range(1000)]
+df["pnr"] = [f"{random.randint(1, 1000000000):010d}" for i in range(10000)]
 df["value1"] = "xzz" + df["pnr"] + "zzx"
 df["value2"] = "3$" + df["pnr"] + ":-xm"
 df.to_csv("c:/temp/xx.csv", sep=";", header=False, index=False)
-for chunk in pd.read_csv(
-    "c:/temp/xx.csv", header=None, dtype=str, sep=";", chunksize=49
-):
+for chunk in pd.read_csv("c:/temp/xx.csv", header=None, dtype=str, sep=";", chunksize=1000):
     for r in chunk.iterrows():
         print(r)
